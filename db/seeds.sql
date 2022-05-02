97ac1c96560524fae6263c196ed1ab6fe30f6d57
Andrew Baird <bairdfix@gmail.com>
2022-05-02 11:48:19 -0500

    messing w/api

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,12 +9,12 @@ SELECT *  FROM department;
    
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("UX lead des", 100,000, 1)
-       ("Product manager", 100,000, 1)
-       ("Sales rep", 100,000, 4)
-       ("Fullstack dev", 100,000,1)
-       ("Marketing lead", 100,000,3)
-       ("Accoutant", 100,000, 2)
+VALUES ("UX lead des", 100000, 1)
+       ("Product manager", 100000, 1)
+       ("Sales rep", 100000, 4)
+       ("Fullstack dev", 100000,1)
+       ("Marketing lead", 100000,3)
+       ("Accoutant", 100000, 2)
 SELECT * FROM role;
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id, department_id)
